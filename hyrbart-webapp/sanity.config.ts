324e3e398e76a7fc794355aa06ed6ef667e25533
Per Almkvist <144068122+peralmkvist@users.noreply.github.com>
2026-09-09 14:09:58 +0200

    Compact and localize Sanity Studio

--- hyrbart-webapp/sanity.config.ts
+++ hyrbart-webapp/sanity.config.ts
@@ -2,7 +2,9 @@
 
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
+import { svSELocale } from '@sanity/locale-sv-se';
 import { schemaTypes } from './sanity/schemaTypes';
+import { CompactField } from './sanity/TranslationToolInput';
 
 export default defineConfig({
   name: 'default',
@@ -10,6 +12,11 @@ export default defineConfig({
   basePath: '/studio',
   projectId: 'djps09z6',
   dataset: 'production',
-  plugins: [structureTool()],
+  plugins: [structureTool(), svSELocale()],
   schema: { types: schemaTypes },
+  form: {
+    components: {
+      field: CompactField,
+    },
+  },
 });
