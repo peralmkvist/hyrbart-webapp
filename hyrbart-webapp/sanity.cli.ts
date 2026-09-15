5ac8eb9a0ef3deccca067372c5b66db62359e1c9
Per Almkvist <144068122+peralmkvist@users.noreply.github.com>
2026-09-15 11:25:18 +0200

    Point Sanity CLI at recovery project

--- hyrbart-webapp/sanity.cli.ts
+++ hyrbart-webapp/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: 'djps09z6',
+    projectId: 'ew1i5o0v',
     dataset: 'production',
   },
 });
